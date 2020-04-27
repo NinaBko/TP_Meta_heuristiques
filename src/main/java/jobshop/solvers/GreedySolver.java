3a48b1f8ba79e5d84d45051cce4d8d4c131e04af
Piouka <nina.batko@gmail.com>
2020-04-27 14:51:10 +0200

    First commit

---
+++ src/main/java/jobshop/solvers/GreedySolver.java
@@ -0,0 +1,18 @@
+package jobshop.solvers;
+
+import jobshop.Instance;
+import jobshop.Result;
+import jobshop.Solver;
+import jobshop.encodings.JobNumbers;
+
+public class GreedySolver implements Solver {
+    @Override
+    public Result solve(Instance instance, long deadline) {
+
+        JobNumbers sol = new JobNumbers(instance);
+
+
+
+        return null;
+    }
+}
